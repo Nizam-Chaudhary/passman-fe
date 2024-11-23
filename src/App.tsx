51a80e31e40ab304ac5aa1fbdeb739e0ee1c5e11
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2024-11-23 19:09:14 +0530

    chore: format code

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,20 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { Button } from './components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from './components/ui/card';
-import { Input } from './components/ui/input';
+} from '@/components/ui/form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Button } from './components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader } from './components/ui/card'
+import { Input } from './components/ui/input'
 
-import { z } from 'zod';
+import { z } from 'zod'
 
 const signUpSchema = z.object({
   userName: z.string().min(3, 'Please Enter user name'),
   email: z.string().email('Please enter valid email'),
   password: z.string().min(10, 'Must contain at least 10 characters'),
-});
+})
 
 function App() {
   const signUpForm = useForm<z.infer<typeof signUpSchema>>({
@@ -33,36 +28,33 @@ function App() {
       email: '',
       password: '',
     },
-  });
+  })
 
   function onSubmit(values: z.infer<typeof signUpSchema>) {
-    console.log('Values: ', values);
+    console.log('Values: ', values)
   }
 
   return (
     <>
       {/* <Card className="w-[30rem] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"> */}
-      <Card className='m-auto min-w-96 w-2/6 my-32'>
-        <CardHeader className='text-center text-2xl'>
+      <Card className="m-auto min-w-96 w-2/6 my-32">
+        <CardHeader className="text-center text-2xl">
           Create your account
         </CardHeader>
         <CardContent>
           <Form {...signUpForm}>
             <form
               onSubmit={signUpForm.handleSubmit(onSubmit)}
-              className='space-y-8'
+              className="space-y-8"
             >
               <FormField
                 control={signUpForm.control}
-                name='userName'
+                name="userName"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Username</FormLabel>
                     <FormControl>
-                      <Input
-                        placeholder='Username'
-                        {...field}
-                      />
+                      <Input placeholder="Username" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -70,16 +62,12 @@ function App() {
               />
               <FormField
                 control={signUpForm.control}
-                name='email'
+                name="email"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                      <Input
-                        placeholder='Email'
-                        type='email'
-                        {...field}
-                      />
+                      <Input placeholder="Email" type="email" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -87,14 +75,14 @@ function App() {
               />
               <FormField
                 control={signUpForm.control}
-                name='password'
+                name="password"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
                       <Input
-                        placeholder='Password'
-                        type='password'
+                        placeholder="Password"
+                        type="password"
                         {...field}
                       />
                     </FormControl>
@@ -102,21 +90,18 @@ function App() {
                   </FormItem>
                 )}
               />
-              <Button
-                className='text-center w-full'
-                type='submit'
-              >
+              <Button className="text-center w-full" type="submit">
                 Sign Up
               </Button>
             </form>
           </Form>
         </CardContent>
-        <CardFooter className='flex justify-center'>
+        <CardFooter className="flex justify-center">
           Already have an account? sign in
         </CardFooter>
       </Card>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
