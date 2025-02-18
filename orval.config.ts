fdd6a57adcc6e2fa816eae39c553cd51d43172ce
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2025-02-19 02:03:45 +0530

    add api-client generation with orval

---
+++ orval.config.ts
@@ -0,0 +1,51 @@
+
+export default {
+  passman: {
+    input: {
+      target: 'http://localhost:3000/openapi.json',
+    },
+    output: {
+      mode: 'split',
+      target: 'src/services/api-client/api.ts',
+      client: 'react-query',
+      httpClient: 'fetch',
+      biome: true,
+      baseUrl: {
+        getBaseUrlFromSpecification: true,
+        variables: {
+          environment: 'api.dev',
+        },
+      },
+      override: {
+        fetch: {
+          includeHttpResponseReturnType: false,
+        },
+        query: {
+          useQuery: true,
+          options: {
+            staleTime: 10000,
+          },
+        },
+        urlEncodeParameters: true,
+      },
+      mock: false,
+    },
+    hooks: {
+      afterAllFilesWrite: 'biome format --write',
+    },
+  },
+  passmanZod: {
+    input: {
+      target: 'http://localhost:3000/openapi.json',
+    },
+    output: {
+      mode: 'split',
+      client: 'zod',
+      target: 'src/services/api-client/api.zod.ts',
+      biome: true
+    },
+    hooks: {
+      afterAllFilesWrite: 'biome format --write',
+    },
+  }
+};
